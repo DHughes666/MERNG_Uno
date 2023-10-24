a54750104c232c3b1139fbac6620946bee399ff7
DHughes666 <graziemolto6@gmail.com>
2023-10-24 13:16:12 +0100

    Schema -Graphql and Models-mongoose successfully created

---
+++ blog-backend/src/schema/schema.ts
@@ -0,0 +1,36 @@
+ import { 
+    GraphQLObjectType, 
+    GraphQLID, 
+    GraphQLString, 
+    GraphQLNonNull,
+} from 'graphql';
+
+
+const UserType = new GraphQLObjectType({
+    name: "UserType",
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString)},
+        password: { type: GraphQLNonNull(GraphQLString)},
+    }),
+});
+
+const BlogType = new GraphQLObjectType({
+    name: "BlogType",
+    fields: () => ({
+        id: { type: GraphQLID},
+        title: { type: GraphQLNonNull(GraphQLString)},
+        content: { type: GraphQLNonNull(GraphQLString) },
+        date: { type: GraphQLNonNull(GraphQLString)},
+    }),
+});
+
+const CommentType = new GraphQLObjectType({
+    name: 'CommentType',
+    fields: () => ({
+        id: { type: GraphQLID},
+        text: { type: GraphQLNonNull(GraphQLString)},
+        
+    }),
+})
